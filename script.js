39068e22d4eddd5d94e9c561758f7fe7c8cafb6b
ChloeBallantyne <160100291+ChloeBallantyne@users.noreply.github.com>
2024-03-20 17:41:55 +1100

    created basic modal and icon for modal

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-//select the btn
 const startButton = document.getElementById("startBtn");
 const startMenu = document.getElementById("menu");
 
-//event listeneer
 startButton.addEventListener("click", () => {
   startMenu.classList.toggle("menu__clicked");
   startButton.classList.toggle("start__clicked");
 });
+
+function updateTime() {
+  let timestamp_UTC = new Date();
+  let readable_timestamp_AEST =
+    timestamp_UTC
+      .toLocaleDateString("en-AU", { timeZone: "Australia/Sydney" })
+      .replace(/\//g, "-") +
+    " " +
+    timestamp_UTC.toLocaleTimeString("en-AU", { timeZone: "Australia/Sydney" });
+  document.getElementById("time").innerText = readable_timestamp_AEST;
+}
+
+setInterval(updateTime, 1000); // Run updateTime() every second
+
+const modalOne = document.getElementById("modalOne");
+const modalBtn = document.getElementById("modalOneBtn");
+const modalOneCloseBtn = document.getElementsByClassName("close")[0];
+
+modalBtn.onclick = function () {
+  modalOne.style.display = "block";
+};
+
+modalOneCloseBtn.onclick = function () {
+  modalOne.style.display = "none";
+};
