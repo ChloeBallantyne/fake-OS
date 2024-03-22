6961824dee022d75e01aa931a6f2bdf553d7611e
ChloeBallantyne <160100291+ChloeBallantyne@users.noreply.github.com>
2024-03-22 17:02:39 +1100

    added modal one music player

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function updateTime() {
   document.getElementById("time").innerText = readable_timestamp_AEST;
 }
 
-setInterval(updateTime, 1000); // Run updateTime() every second
+setInterval(updateTime, 1000);
 
 const modalOne = document.getElementById("modalOne");
 const modalBtn = document.getElementById("modalOneBtn");
@@ -29,4 +29,23 @@ modalBtn.onclick = function () {
 
 modalOneCloseBtn.onclick = function () {
   modalOne.style.display = "none";
+};
+
+const list = document.getElementById("list");
+
+list.onclick = function (e) {
+  e.preventDefault();
+  let elm = e.target;
+  let audio = document.getElementById("audio");
+  let source = document.getElementById("audioSource");
+  source.src = elm.getAttribute("data-value");
+  let songsList = document.getElementsByClassName("song");
+  for (let i = 0; i < songsList.length; i++) {
+    if (songsList[i].classList.contains("song__selected")) {
+      songsList[i].classList.remove("song__selected");
+    }
+  }
+  elm.classList.add("song__selected");
+  audio.load();
+  audio.play();
 };
